411c2256c81e87c18302f236bbfe1e518912e590
Ivy-Steph <stephieivy@gmail.com>
2023-09-03 13:11:39 +0200

    adding fixture, and implementing get total

--- shopping_cart.py
+++ shopping_cart.py
@@ -2,10 +2,13 @@ from typing import List
 
 
 class ShoppingCart:
-    def __init__(self) -> None:
+    def __init__(self, max_size: int) -> None:
        self.items: list[str] = []
+       self.max_size = max_size
 
     def add(self, item: str):
+        if self.size() == self.max_size:
+            raise OverflowError("Cannot add more items")
         self.items.append(item)
 
     def size(self) -> int:
@@ -15,4 +18,7 @@ class ShoppingCart:
         return self.items
 
     def get_total_price(self, price_map):
-        pass
+        total_price = 0
+        for item in self.items:
+            total_price += price_map.get(item)
+        return total_price
--- test_shopping_cart.py
+++ test_shopping_cart.py
@@ -1,11 +1,29 @@
 from shopping_cart import ShoppingCart
+import pytest
 
-def test_can_add_item_to_cart():
-	cart = ShoppingCart()
+@pytest.fixture
+def cart():
+	#all set up for cart here
+	return ShoppingCart(5)
+
+def test_can_add_item_to_cart(cart):
 	cart.add("apple")
 	assert cart.size() == 1 #asserts 1 item has been added
 
-def test_cart_contains_item_when_item_appended():
-	cart = ShoppingCart()
+def test_cart_contains_item_when_item_appended(cart):
 	cart.add("apple")
 	assert "apple" in cart.get_items() #checks if item "apple" is in cart
+
+def test_add_more_than_max_items_should_fail(cart):
+	for _ in range(5):
+		cart.add("apple")
+	with pytest.raises(OverflowError):
+		cart.add("apple")
+
+def test_get_total_price(cart):
+	cart.add("apple")
+	cart.add("orange")
+
+	price_map = {"apple": 1.0, "orange": 2.0}
+
+	assert cart.get_total_price(price_map) == 3.0
